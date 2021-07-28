18189a0ca6d3aef6d2271920a9677b339421c5df
wingsmight <wingsmight@gmail.com>
2021-07-28 23:41:37 +0700

    show Navigation title on any iOS verion

--- LearnCodeAssembly/Views/SourceCodeFileListView.swift
+++ LearnCodeAssembly/Views/SourceCodeFileListView.swift
@@ -2,25 +2,55 @@ import SwiftUI
 
 struct SourceCodeFileListView: View {
     var body: some View {
-            NavigationView {
-                List(sourceCodeFiles) { sourceCodeFile in
-                    ZStack {
-                        SourceCodeFileView(sourceCodeFile: sourceCodeFile)
-                        NavigationLink(destination: CodeEditor()) {
-                            EmptyView()
-                        }
-                        .opacity(/*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(sourceCodeFiles) { sourceCodeFile in
+                ZStack {
+                    SourceCodeFileView(sourceCodeFile: sourceCodeFile)
+                    NavigationLink(destination: CodeEditor()) {
+                        EmptyView()
                     }
+                    .opacity(/*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
                 }
             }
-            .navigationBarTitle("Files", displayMode: .inline)
-            //.navigationTitle("Files")
+            .supportedNavigationTitle(with: "Files")
+        }
     }
 }
 
 struct SourceCodeFileListView_Previews: PreviewProvider {
     static var previews: some View {
         SourceCodeFileListView()
-            .preferredColorScheme(.light)
+            .preferredColorScheme(.dark)
+    }
+}
+
+
+@available(iOS 14, *)
+struct NavigationTitleViewModifier: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        content
+            .navigationTitle(text)
+    }
+}
+struct NavigationBarTitleViewModifier: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        content
+            .navigationBarTitle(text)
+    }
+}
+extension View {
+    @ViewBuilder
+    func supportedNavigationTitle(with text: String) -> some View {
+        if #available(iOS 14, *) {
+            self
+                .modifier(NavigationTitleViewModifier(text: text))
+        }
+        else {
+            self.modifier(NavigationBarTitleViewModifier(text: text))
+        }
     }
 }
